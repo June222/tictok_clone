6bd170e66797d00b517e452d1a2bb38982f9e2a5
June222 <38900003+June222@users.noreply.github.com>
2023-02-11 14:16:03 +0900

    textTheme 생성 https://m2.material.io/design/typography/the-type-system.html#type-scale

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tictok_clone/features/navigation/main_navigation_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tictok_clone/features/authentication/widgets/login_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -26,6 +27,67 @@ class TikTokApp extends StatelessWidget {
           foregroundColor: Colors.black,
           centerTitle: true,
         ),
+        textTheme: TextTheme(
+          displayLarge: GoogleFonts.raleway(
+            fontSize: 99,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -1.5,
+          ),
+          displayMedium: GoogleFonts.raleway(
+            fontSize: 62,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -0.5,
+          ),
+          displaySmall:
+              GoogleFonts.raleway(fontSize: 49, fontWeight: FontWeight.w400),
+          headlineMedium: GoogleFonts.raleway(
+            fontSize: 35,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          headlineSmall:
+              GoogleFonts.raleway(fontSize: 25, fontWeight: FontWeight.w400),
+          titleLarge: GoogleFonts.raleway(
+            fontSize: 21,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.15,
+          ),
+          titleMedium: GoogleFonts.raleway(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.15,
+          ),
+          titleSmall: GoogleFonts.raleway(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.1,
+          ),
+          bodyLarge: GoogleFonts.splineSans(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.5,
+          ),
+          bodyMedium: GoogleFonts.splineSans(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          labelLarge: GoogleFonts.splineSans(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 1.25,
+          ),
+          bodySmall: GoogleFonts.splineSans(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.4,
+          ),
+          labelSmall: GoogleFonts.splineSans(
+            fontSize: 10,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 1.5,
+          ),
+        ),
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
@@ -33,7 +95,7 @@ class TikTokApp extends StatelessWidget {
         primaryColor: const Color.fromARGB(179, 241, 71, 41),
         bottomAppBarTheme: BottomAppBarTheme(color: Colors.grey.shade900),
       ),
-      home: const MainNavigationScreen(),
+      home: const LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
