a43ab461ac1a2679ab4b9dbb9af940fee2ab9f25
June222 <38900003+June222@users.noreply.github.com>
2023-02-27 14:59:32 +0900

    #19.3 selfie mode 설정하기

--- lib/features/vidoes/video_recording_screen.dart
+++ lib/features/vidoes/video_recording_screen.dart
@@ -13,8 +13,10 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
-  late final CameraController _cameraController; // 7. late로 카메라 Controller 선언
+  late CameraController
+      _cameraController; // 7. late로 카메라 Controller 선언 // 14. 재정의되면 안되므로 final 삭제
   bool _hasPermission = false;
+  bool _isSelfiMode = false;
 
   @override
   void initState() {
@@ -53,7 +55,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
 
     _cameraController = CameraController(
       // 8. 사용 가능한 카메라 중에서 하나를 골라 연결 => 그래서 late로 선언
-      camerasDescription[0],
+      camerasDescription[_isSelfiMode ? 1 : 0],
       ResolutionPreset.ultraHigh,
       // 촬영될 이미지의 화질 설정
       // Affect the quality of video recording and image capture:
@@ -63,34 +65,45 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     await _cameraController.initialize(); // 9. camera init
   }
 
+  Future<void> toggleSefieMode() async {
+    _isSelfiMode = !_isSelfiMode;
+    await initCamera(); // 12. 다시 카메라 실행
+    setState(() {}); // 13. 화면에 출력
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: SizedBox(
-          width: size.width,
-          height: size.height,
-          child: !_hasPermission ||
-                  !_cameraController.value
-                      .isInitialized // 10. 권한 부여 확인 및, camera 작동 준비 완료 확인하고 실행
-              ? const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Initializing Camera..."),
-                    Gaps.v20,
-                    CircularProgressIndicator.adaptive(),
-                  ],
-                )
-              : Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    CameraPreview(
-                        _cameraController), // 11. camera에 촬영되는 이미지는 CameraPreview로 확인
-                  ],
-                ),
-        ),
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: !_hasPermission ||
+                !_cameraController.value
+                    .isInitialized // 10. 권한 부여 확인 및, camera 작동 준비 완료 확인하고 실행
+            ? const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Initializing Camera..."),
+                  Gaps.v20,
+                  CircularProgressIndicator.adaptive(),
+                ],
+              )
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(
+                      _cameraController), // 11. camera에 촬영되는 이미지는 CameraPreview로 확인
+                  Positioned(
+                    top: 100,
+                    child: IconButton(
+                      onPressed: toggleSefieMode,
+                      icon: const Icon(Icons.cameraswitch),
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
