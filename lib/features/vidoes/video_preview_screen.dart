9749143c5295624027c698ef26bfe41ab815bec4
June222 <38900003+June222@users.noreply.github.com>
2023-02-27 18:51:30 +0900

    저장한 비디오 파일을 화면에 띄우는 방법

--- lib/features/vidoes/video_preview_screen.dart
+++ lib/features/vidoes/video_preview_screen.dart
@@ -7,18 +7,18 @@ import 'package:video_player/video_player.dart';
 
 class VideoPreviewScreen extends StatefulWidget {
   const VideoPreviewScreen({super.key, required this.video});
-  final XFile video;
+  final XFile video; // 전달 받은 비디오 파일 정보
   @override
   State<VideoPreviewScreen> createState() => _VideoPreviewScreenState();
 }
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
-  late final VideoPlayerController _videoPlayerController;
+  late final VideoPlayerController _videoPlayerController; // 실행시키기 위한 비디오 컨트롤러
 
   @override
   void initState() {
     super.initState();
-    initVideoPlayerController();
+    initVideoPlayerController(); // 다른 컨트롤러와 마찬가지로 initState시에 initialize 해주어야함.
   }
 
   @override
@@ -29,14 +29,14 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
 
   Future<void> initVideoPlayerController() async {
     _videoPlayerController = VideoPlayerController.file(
-      File(widget.video.path),
+      File(widget.video.path), // 실행시킬 비디오 파일의 directory를 설정!!
     );
 
     await _videoPlayerController.initialize();
     await _videoPlayerController.setLooping(true);
     await _videoPlayerController.play();
 
-    setState(() {});
+    setState(() {}); // 이거 하지않으면 화면 재생이 안됨.
   }
 
   @override
@@ -45,7 +45,8 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text("Preview Video")),
       body: _videoPlayerController.value.isInitialized
-          ? VideoPlayer(_videoPlayerController)
+          ? VideoPlayer(
+              _videoPlayerController) // type(VideoPlayer) == Widget 임.
           : const Text("No Video"),
     );
   }
